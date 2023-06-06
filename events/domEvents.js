4c3d082abe67d74c943954b0004db9a30a4387a2
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-06-06 13:30:23 -0500

    fixed bug. cards returning id

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,5 +1,15 @@
 import { signOut } from '../utils/auth';
 import clearDom from '../utils/clearDom';
+import {
+  getSingleWord,
+  getUserWords,
+  getPublicWords,
+  createWord,
+  updateWord,
+  publicFilterByLanguage,
+  userFilterByLanguage,
+  deleteWord,
+} from '../utils/databaseCalls/wordData';
 
 const domEvents = (user) => {
   const signOutEventListener = () => {
@@ -16,12 +26,15 @@ const domEvents = (user) => {
         if (e.target.id.includes('delete-user')) {
           const [, firebaseKey] = e.target.id.split('--');
           await deleteWord(firebaseKey);
-          await getUserWords(user.uid);
+          await getUserWords(user);
         }
         if (e.target.id.includes('delete-community')) {
           const [, firebaseKey] = e.target.id.split('--');
           await deleteWord(firebaseKey);
-          await getCommunityWords(user.uid);
+          await getCommunityWords(user);
+        }
+        if (e.target.id.includes('edit-user')) {
+          const [, firebaseKey] = e.target.id.split('--');
         }
       });
   };
