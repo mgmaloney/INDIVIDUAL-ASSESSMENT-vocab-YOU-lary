93cdb2dcef28a9670a07de23c95486203ded9243
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-06-08 13:02:33 -0500

    add language modal box working

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,4 @@
-import { filterBtnsCommunity } from '../components/filtersBtns';
+import { filterBtnsCommunity, filterBtnsUser } from '../components/filtersBtns';
 import navBar from '../components/navbar';
 import {
   getCommunityWords,
@@ -9,12 +9,13 @@ const navigationEvents = (user) => {
   document.getElementById('navigation').addEventListener('click', async (e) => {
     if (e.target.id === 'community-btn') {
       await getCommunityWords();
-      await filterBtnsCommunity();
       navBar(true);
+      await filterBtnsCommunity();
     }
     if (e.target.id.includes('home-btn')) {
       await getUserWords(user);
       navBar(false);
+      await filterBtnsUser();
     }
   });
 };
